c17f1d277034e43c5e733798e265938848656511
rahulgudla1999 <52357597+rahulgudla1999@users.noreply.github.com>
2022-10-03 12:58:28 +0530

    Add files via upload

---
+++ CalculateExpression.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            try
+            {
+                Console.WriteLine("The value is " + p.FindX(7, 1).ToString());
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Divide by zero is not possible " + ex.Message);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot get the value.. some wierd occured " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public double FindX(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)Math.Sqrt((3 * a) + 1) / b;
+        }
+    }
+}
---
+++ MaxElementInArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxElementInArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if (n <= 0)
+                {
+                    throw new Exception("please enter n value greater than zero");
+                }
+                int[] array = new int[n];
+                if (n > 1)
+                {
+                    Console.WriteLine("Enter elements of array....");
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        array[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                int max = array[0];
+                for (int i = 0; i < n; i++)
+                {
+                    if (max < array[i])
+                        max = array[i];
+                }
+                Console.WriteLine("Maximum element in array is " + max);
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.Write("Exception - " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
